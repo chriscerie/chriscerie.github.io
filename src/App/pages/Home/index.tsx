ab0dbfa465425cc37abb9bedce92820231f890fa
Krysmatic <51393127+Krysiene@users.noreply.github.com>
2021-07-28 23:08:26 -0700

    Add home page

---
+++ src/App/pages/Home/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.css'
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="middle-horizontal-container">
+        <h1>Hi, I'm Chris.</h1>
+        <h2>A software engineer.</h2>
+      </div>
+    </div>
+  )
+}
+
+export default Home
