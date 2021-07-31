5af4453233b4727c440066cc78af1c0517d68336
Krysmatic <51393127+Krysiene@users.noreply.github.com>
2021-07-31 13:25:42 -0700

    Style topbar links

--- src/App/components/Header/LinksList.tsx
+++ src/App/components/Header/LinksList.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import "./index.css";
+import React, { useEffect, useState } from "react";
+import "./LinksList.css";
 import Breadcrumbs from "@material-ui/core/Breadcrumbs";
 import Link from "@material-ui/core/Link";
 
@@ -15,6 +15,27 @@ type propTypes = {
 };
 
 function LinksList(props: propTypes) {
+  const [currentLink, setCurrentLink] = useState(props.linkRefs[0].linkName)
+
+  // Set currentLink to current page in viewport
+  useEffect(() => {
+    const handleScroll = () => {
+      for (const linkInfo of props.linkRefs) {
+        const yPosition = linkInfo.linkRef.current?.getBoundingClientRect().y;
+        if (yPosition && yPosition <= 120 && yPosition >= -120) {
+          if (linkInfo.linkName !== currentLink) {
+            setCurrentLink(linkInfo.linkName)
+          }
+        }
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [props.linkRefs, currentLink]);
+
   return (
     <Breadcrumbs aria-label="breadcrumb" separator="|" style={{ float: "right", transform: "translate(0%, 50%)" }}>
       {props.linkRefs.map(
@@ -22,7 +43,7 @@ function LinksList(props: propTypes) {
           linkName: string;
           linkRef: React.RefObject<HTMLDivElement>;
         }) => (
-          <Link color="inherit" onClick={ onClick(value.linkRef) }>{ value.linkName }</Link>
+          <div className={currentLink === value.linkName ? "currentBox" : "linkBox"} onClick={ onClick(value.linkRef) }>{value.linkName}</div>
         )
       )}
     </Breadcrumbs>
