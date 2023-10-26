b6a51fa304fd1642c82b332b7bb869c78c18369e
Christopher Chang <51393127+chriscerie@users.noreply.github.com>
2023-10-26 13:29:00 -0700

    Sunset website

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,18 +1,12 @@
 import React, { useRef } from 'react';
 import './index.css';
-import About from './pages/About';
 import Home from './pages/Home';
-import Employment from './pages/Employment';
-import Projects from './pages/Projects';
 import Footer from './pages/Footer';
 import Header from './components/Header';
 import HorizontalBar from './components/HorizontalBar';
 
 function App() {
   const homeRef = useRef<HTMLDivElement>(null);
-  const aboutRef = useRef<HTMLDivElement>(null);
-  const employmentRef = useRef<HTMLDivElement>(null);
-  const projectsRef = useRef<HTMLDivElement>(null);
 
   return (
     <div className="App">
@@ -20,15 +14,6 @@ function App() {
         {
           linkName: "Home",
           linkRef: homeRef,
-        }, {
-          linkName: "About",
-          linkRef: aboutRef,
-        }, {
-          linkName: "Employment",
-          linkRef: employmentRef,
-        }, {
-          linkName: "Projects",
-          linkRef: projectsRef,
         },
       ]}/>
       <Home homeRef={ homeRef }/>
@@ -37,11 +22,6 @@ function App() {
           <img src="/images/circle-arrow.ico" alt="Arrow" />
         </div>
       </HorizontalBar>
-      <About aboutRef={ aboutRef }/>
-      <HorizontalBar direction="up" />
-      <Employment employmentRef={ employmentRef }/>
-      <HorizontalBar direction="down" />
-      <Projects projectsRef={ projectsRef }/>
       <Footer />
     </div>
   );
